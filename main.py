87048ca35e69101ec4138a10830bf476223dfa1a
MarkShi17 <68798552+MarkShi17@users.noreply.github.com>
2023-12-16 17:02:07 -0500

    Update main.py

--- main.py
+++ main.py
@@ -7,9 +7,8 @@ def draw_circles_with_converging_rings(screen, circles):
     pygame.init()
     pygame.mixer.init()
     clock = pygame.time.Clock()
-    font = pygame.font.Font(None, 36)  # Font for the numbers
+    font = pygame.font.Font(None, 36)
     hit_sound = pygame.mixer.Sound('E:\Mark\Progamming\Python\Projects\quhacks2023\soft-hitsoft.wav')
-    # Initialize the score
     score = 0
     combo=1
     max_combo=0
@@ -22,11 +21,9 @@ def draw_circles_with_converging_rings(screen, circles):
                 pygame.quit()
                 # if combo > max_combo:
                 #     max_combo = combo
-                # return [score, max_combo, hits, miss]  # Return the score when the game ends
+                # return [score, max_combo, hits, miss]
             elif event.type == pygame.MOUSEBUTTONDOWN:
-                # User clicks the mouse. Get the position
                 pos = pygame.mouse.get_pos()
-                # Check if the click is within any of the circles
                 for circle in circles:
                     dx = pos[0] - circle['pos'][0]
                     dy = pos[1] - circle['pos'][1]
@@ -50,7 +47,7 @@ def draw_circles_with_converging_rings(screen, circles):
                             hits += 1
                             hit_sound.play()
                             #print('inner')
-                        circle['ring_radius'] = 0  # Also hide the ring
+                        circle['ring_radius'] = 0
                         combo +=1
             elif event.type == pygame.KEYDOWN:
                 if event.key == pygame.K_z or event.key == pygame.K_x:
@@ -74,16 +71,13 @@ def draw_circles_with_converging_rings(screen, circles):
                                 score += 300 * combo
                                 hits += 1
                                 hit_sound.play()
-                            circle['ring_radius'] = 0  # Also hide the ring
+                            circle['ring_radius'] = 0
                             combo += 1
-        screen.fill((0, 0, 0))  # Fill the screen with black
+        screen.fill((0, 0, 0))
 
         for i, circle in enumerate(circles):
-            # Delay before drawing the circle and its ring
             if pygame.time.get_ticks() < circle['start_time']:
                 continue
-
-            # Draw the main circle and its ring if the flag is set
             if circle['draw_circle']:
                 pygame.draw.circle(screen, circle['color'], circle['pos'], circle['radius'])
                 num = circle['local_num']
@@ -110,7 +104,7 @@ def draw_circles_with_converging_rings(screen, circles):
             #finished = False
             if combo > max_combo:
                 max_combo = combo
-            return [score, max_combo, hits, miss]  # Return the score when the game ends
+            return [score, max_combo, hits, miss]
 def random_circles(time, num):
     circles=[]
     past_x = 0
@@ -144,27 +138,18 @@ def random_circles(time, num):
         past_y = ypos
     return circles
 def end_screen(info):
-    screen.fill((0, 255, 0))  # Fill the screen with green
-    font = pygame.font.Font(None, 72)  # Font for the game statistics
+    screen.fill((0, 255, 0))
+    font = pygame.font.Font(None, 72)
     text = font.render(f"Score: {info[0]}", True, (0, 0, 0))
     screen.blit(text, (200, 100))
     text = font.render(f"Max Combo: {info[1]}", True, (0, 0, 0))
     screen.blit(text, (200, 200))
     text = font.render(f"Hits: {info[2]} Misses: {info[3]}", True, (0, 0, 0))
     screen.blit(text, (200, 300))
-    accuracy = info[2] / (info[2] + info[3]) * 100  # Calculate the accuracy
+    accuracy = info[2] / (info[2] + info[3]) * 100
     text = font.render(f"Accuracy: {accuracy:.1f}%", True, (0, 0, 0))
     screen.blit(text, (200, 400))
     pygame.display.flip()
-
-# Define your circles here
-# circles = [
-#     {'color': (255, 255, 255), 'pos': (400, 300), 'radius': 50, 'ring_radius': 100, 'speed': 1, 'draw_circle': True, 'start_time': 0},
-#     {'color': (255, 255, 255), 'pos': (100, 100), 'radius': 50, 'ring_radius': 100, 'speed': 1, 'draw_circle': True, 'start_time': 1000},
-#     {'color': (255, 255, 255), 'pos': (700, 100), 'radius': 50, 'ring_radius': 100, 'speed': 1, 'draw_circle': True, 'start_time': 2000},
-#     {'color': (255, 255, 255), 'pos': (100, 500), 'radius': 50, 'ring_radius': 100, 'speed': 1, 'draw_circle': True, 'start_time': 3000},
-#     {'color': (255, 255, 255), 'pos': (700, 500), 'radius': 50, 'ring_radius': 100, 'speed': 1, 'draw_circle': True, 'start_time': 4000}
-# ]
 
 
 pygame.init()
@@ -197,13 +182,11 @@ while running:
                         state = random_game
     pygame.display.flip()
     if state == title:
-        # Draw title screen
         screen.fill("green")
         for button in buttons:
             rect = pygame.draw.rect(screen, (255, 0, 0),(button["pos"][0], button["pos"][1], 300, 100))
-            button["rect"] = rect  # Store the Rect object for later
+            button["rect"] = rect
 
-            # Draw the text
             font = pygame.font.Font(None, 24)
             label = font.render(button["label"], True, (255, 255, 255))
             screen.blit(label, (button["pos"][0] + 10, button["pos"][1] + 10))
@@ -244,11 +227,9 @@ while running:
     #     info = draw_circles_with_converging_rings(screen, circles)
     #     state = end
     elif state == end:
-        # Draw end screen
         font = pygame.font.Font(None, 36)
         end_screen(info)
 
-        # Check for mouse click to go back to the title screen
         if event.type == pygame.MOUSEBUTTONDOWN:
             state = title
             time.sleep(0.1)
